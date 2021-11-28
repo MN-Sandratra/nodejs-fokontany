5c35c033607efb6ed59f46a4433083cf69187121
Harry Michael <ando.lalaina1103@gmail.com>
2021-11-28 16:40:36 +0300

    Refactor

--- src/server.ts
+++ src/server.ts
@@ -1,39 +1,33 @@
-import express from 'express'
-
-const mongoose = require('mongoose')
 require('dotenv/config')
 
-const server=express();
-const port=3030;
-
+const express = require('express')
+const mongoose = require('mongoose')
 const userRoute = require('./routes/user.route')
 const fokontanyRoute = require('./routes/fokontany.route')
 
-server.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
-server.use(express.json());
-server.use(express.urlencoded({ extended: true }))
+const PORT = 3030;
 
-//middleware route
-server.use('/api/users', userRoute);
-server.use('/api/fokontany', fokontanyRoute);
+mongoose.connect(process.env.CONNECTION_DB, {useNewUrlParser:true, useUnifiedTopology: true})
+    .then(() => {
+        const server = express();
+        console.log("Connected to MongoDB Database")
 
-server.get('/',(req,res)=>{
-    res.send('Hello world')
-});
-async function startServer(){
-    await mongoose.connect(process.env.CONNECTION_DB,
-        {useNewUrlParser:true,
-         useUnifiedTopology: true },
-        ).then(()=>console.log("Connected to MongoDB Database"))
-        .catch(()=>console.log("tsy mety eh"));
-    server.listen(port,()=>console.log(
-        `server running at http://localhost:${port}`
-    ));
-}
+        server.use((req, res, next) => {
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+            next();
+        });
+        server.use(express.json());
+        server.use(express.urlencoded({ extended: true }))
 
-startServer();
+        server.use('/api/users', userRoute);
+        server.use('/api/fokontany', fokontanyRoute);
+        
+        server.get('/',(req,res)=>{
+            res.send('Hello world')
+        });
+
+        server.listen(PORT, () => console.log(`server running at http://localhost:${PORT}`));
+    })
+    .catch((err) => console.log(err));
